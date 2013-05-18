148e594083b35b4580d968b20d840f9abfb950a0
Traiko Dinev <traiko.dinev@gmail.com>
2013-05-18 09:39:54 +0300

    Test2

--- ClassAssessment/ClassAssessment/Controllers/TestController.cs
+++ ClassAssessment/ClassAssessment/Controllers/TestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace ClassAssessment.Controllers
 {
@@ -11,10 +12,27 @@ namespace ClassAssessment.Controllers
 		//
 		// GET: /Test/
 
+		[Authorize]
 		public string Index()
 		{
+			FormsAuthentication.SignOut();
 			return "Fuck you, world";
 		}
 
+		[HttpPost]
+		public ActionResult Login(string inputName, string inputPassword)
+		{
+			if (inputName == "asdf" && inputPassword == "asdf")
+			{
+				FormsAuthentication.SetAuthCookie(inputName, true);
+				return RedirectToAction("Index");
+			}
+			else return View("~/Views/Login.cshtml");
+		}
+
+		public ActionResult Login()
+		{
+			return View("~/Views/Login.cshtml");
+		}
 	}
 }
