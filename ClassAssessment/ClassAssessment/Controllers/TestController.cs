f94203faab48d3ebd335d0aefe7f8d855a664918
Traiko Dinev <traiko.dinev@gmail.com>
2013-05-18 08:53:27 +0300

    Appharbor test

---
+++ ClassAssessment/ClassAssessment/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClassAssessment.Controllers
+{
+	public class TestController : Controller
+	{
+		//
+		// GET: /Test/
+
+		public string Index()
+		{
+			return "Fuck you, world";
+		}
+
+	}
+}
