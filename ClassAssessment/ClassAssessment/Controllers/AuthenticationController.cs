83f46e8dd141798df88102aae50a9f7eb8611d2a
Traiko Dinev <traiko.dinev@gmail.com>
2013-05-24 16:37:40 +0300

    Working insert and modify
    With this commit we are one step closer to ruling the world... someday

--- ClassAssessment/ClassAssessment/Controllers/AuthenticationController.cs
+++ ClassAssessment/ClassAssessment/Controllers/AuthenticationController.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using ClassAssessment.Models;
+using HttpContext = System.Web.HttpContext;
 
 namespace ClassAssessment.Controllers
 {
 	public class AuthenticationController : Controller
 	{
+		private ClassAssessment.Models.db92115aa68afb4039a247a1c20130a248Entities DBContext =
+			new db92115aa68afb4039a247a1c20130a248Entities();
+
 		[Authorize]
 		public ActionResult Logout()
 		{
@@ -20,18 +25,32 @@ namespace ClassAssessment.Controllers
 		public ActionResult Login(string inputName, string inputPassword)
 		{
 			ViewBag.Username = inputName;
-			if (inputName == "asdf" && inputPassword == "asdf")
+			var user = (from x in DBContext.Users
+					   where x.Name == inputName
+					   select x).FirstOrDefault();
+
+			if (user == null)
+				return View();
+
+			//if (inputName == "asdf" && inputPassword == "asdf")
 			{
-				FormsAuthentication.SetAuthCookie(inputName, true);
+				FormsAuthentication.SetAuthCookie(user.Name + " " + user.Surname, true);
 
 				return RedirectToAction("Index", "Default");
 			}
-			else return View();
 		}
 
 		public ActionResult Login()
 		{
+			if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
+				return RedirectToAction("Index", "Default");
 			return View();
+		}
+
+		[Authorize(Users="Трайко Динев")]
+		public string Dummy()
+		{
+			return "fuck yeah";
 		}
 	}
 }
--- ClassAssessment/ClassAssessment/Controllers/DefaultController.cs
+++ ClassAssessment/ClassAssessment/Controllers/DefaultController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using ClassAssessment.Models;
+using System.Diagnostics;
+using System.Collections.Specialized;
 
 namespace ClassAssessment.Controllers
 {
@@ -13,27 +16,134 @@ namespace ClassAssessment.Controllers
         // GET: /Default/
 		private ClassAssessment.Models.db92115aa68afb4039a247a1c20130a248Entities DBContext =
 			new db92115aa68afb4039a247a1c20130a248Entities();
+	
+		public class NameShort
+		{
+			public int Id;
+			public string Name, Surname;
+		}
+
+		public class QuestionShort
+		{
+			public int id;
+			public string Text;
+		}
 
 		[Authorize]
         public ActionResult Index(int userId = 0)
         {
-			//Database
-			ViewBag.Users = DBContext.Users;
-			ViewBag.Questions = DBContext.Questions;
+			//get id
+			string name = HttpContext.User.Identity.Name.Split()[0];
+			var currentUser = (from user in DBContext.Users
+					  where user.Name == name
+					  select user).First();
+
+			ViewBag.Users = from user in DBContext.Users
+							orderby user.Name
+							select new NameShort
+							{
+								Id = user.Id,
+								Name = user.Name,
+								Surname = user.Surname
+							};
+
+            ViewBag.Rated = from assessment in currentUser.Assessments
+                            join user in DBContext.Users
+                            on assessment.To equals user.Id
+                            select new NameShort
+                            {
+                                Id = user.Id,
+                                Name = user.Name,
+                                Surname = user.Surname
+                            };
+			
+			ViewBag.Questions = (from x in DBContext.Questions
+                                orderby x.id
+								select new QuestionShort
+								{
+									id = x.id,
+									Text = x.Text
+								}).ToList();
+
+            ViewBag.Answers = (from answer in DBContext.Answers
+                              where answer.Assessments.From == currentUser.Id && answer.Assessments.To == userId
+                              orderby answer.Questions.id
+                              select answer.Value).ToList();
+
 			ViewBag.SelectedUser = userId;
 
             return View();
         }
 
+        private int InsertAssessment(Users currentUser, int assessedUserId, NameValueCollection answers)
+        {
+            var assessment = new Assessments()
+            {
+                From = currentUser.Id,
+                To = assessedUserId,
+            };
+
+            DBContext.Assessments.Add(assessment);
+
+            //questions
+            for (int question = 1; true; question++)
+            {
+                var value = answers[question.ToString()];
+                if (string.IsNullOrEmpty(value))
+                    break;
+
+                //they are ordered by id, so the first one is really the first one in the database
+                var newAnswer = new Answers()
+                {
+                    Assessment = assessment.id,
+                    Question = question,
+                    Value = int.Parse(value)
+                };
+
+                DBContext.Answers.Add(newAnswer);
+            }
+
+            return DBContext.SaveChanges();
+        }
+
+        private int UpdateAssessment(Assessments assessment, Users currentUser, int assessedUserId, NameValueCollection answers)
+        {
+            for (int question = 1; true; question++)
+            {
+                var value = answers[question.ToString()];
+                if (string.IsNullOrEmpty(value))
+                    break;
+
+                var ans = assessment.Answers.First((answer) => answer.Question == question);
+                ans.Value = int.Parse(value);
+
+            }
+
+            return DBContext.SaveChanges();
+        }
+
 		[Authorize]
 		[HttpPost]
-		public ActionResult Index()
+		public ActionResult Index(int userId = 0, int shit = 1)
 		{
-			ViewBag.Users = DBContext.Users;
-			ViewBag.Questions = DBContext.Questions;
-			ViewBag.SelectedUser = 0;
+			//get id
+			string name = HttpContext.User.Identity.Name.Split()[0];
+			var currentUser = (from x in DBContext.Users
+					  where x.Name == name
+					  select x).First();
 
-			return View();
+            var previousAssessment = (from assessment in currentUser.Assessments
+                                      where assessment.To == userId
+                                      select assessment).FirstOrDefault();
+
+            if (previousAssessment == null)
+                InsertAssessment(currentUser, userId, Request.Form);
+            else UpdateAssessment(previousAssessment, currentUser, userId, Request.Form);
+
+            
+            //int result = DBContext.SaveChanges();
+
+			return Index(userId);
 		}
     }
 }
